c7ebadbab03b56427f26013b9bbb4e8df4edc9bb
Ildikó Cseri <cseriildiko@icloud.com>
2025-03-04 13:23:11 +0100

    ✨ feat: add script to set up the same configs on new environment

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+DIR="$(realpath "$(dirname "$BASH_SOURCE")")"
+
+backup_and_create_link() {
+  if [ -e "$HOME/$1" ]; then
+    if ! diff "$HOME/$1" "$DIR/$1" > /dev/null; then
+      mv "$HOME/$1" "$HOME/${1}_backup"
+    else
+      rm "$HOME/$1"
+    fi
+  fi
+  ln -s "$DIR/$1" "$HOME/$1"
+  ls -la "$HOME/$1"
+}
+
+backup_and_create_link .zshrc
+backup_and_create_link .zsh/functions.zsh
+backup_and_create_link .zsh/aliases.zsh
